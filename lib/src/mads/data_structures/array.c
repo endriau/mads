7bc485583b961539d6460fe79fafc56c63c34969
Endri Kastrati <endriau@gmail.com>
2024-06-10 15:16:59 +1000

    Added documentation for array source file.

--- lib/src/mads/data_structures/array.c
+++ lib/src/mads/data_structures/array.c
@@ -1,51 +1,89 @@
 // ReSharper disable CppDFANullDereference
 
 
+
+// Including the necessary libraries.
+// stdio.h is included for input/output operations.
+// stdlib.h is included for dynamic memory allocation.
+// assert.h is included to provide a macro called assert() which can be used to verify assumptions made by the program
+// and print a diagnostic message if this assumption is false.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
-
+// Including the header file for mads_array_t.
 #include <mads/data_structures/array.h>
 
-
+// The function for creating a new mads array. It takes three function pointers as parameters.
+// comparator function is used to compare 2 data elements. It should return 0 if both elements are equal,
+// negative number if element-1 is smaller and positive number if element-1 is larger.
+// printer function is used to print a data element.
+// destructor function is used to deallocate memory for a data element.
 mads_array_t *mads_array_create(const mads_array_comparator_fn comparator, const mads_array_printer_fn printer, const mads_array_destructor_fn destructor)
 {
+    // Initialize pointer to the new array as NULL.
     mads_array_t *new_array = NULL;
+
+    // Set the initial size for the array.
     const long long int initial_msize = 16;
+
+    // Check that both comparator and printer functions are provided. If not, throw an assertion error.
     assert(comparator != NULL && printer != NULL);
+
+    // Allocate memory for the mads array. If memory allocation fails, an assertion error is thrown.
     new_array = (mads_array_t *)malloc(sizeof(*new_array));
     assert(new_array != NULL);
+
+    // Allocate memory for the array blocks. If memory allocation fails, an assertion error is thrown.
     new_array->mblocks = (void **)malloc(initial_msize * sizeof(void *));
     assert(new_array->mblocks != NULL);
+
+    // Set the array size as initial size.
     new_array->msize = initial_msize;
+
+    // Set the initial index for the array as -1 (empty).
     new_array->index = -1;
+
+    // Attach the comparator, printer and destructor functions to the array object.
     new_array->comparator = comparator;
     new_array->printer = printer;
     new_array->destructor = destructor;
+
+    // Return the newly created array.
     return new_array;
 }
 
-
+// The function that appends data to the array.
 void mads_array_append(mads_array_t *array, void *data)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
+
+    // Insert the data at the end of the array.
     mads_array_insert_at(array, array->index + 1, data);
 }
 
-
+// The function that prepends data to the array.
 void mads_array_prepend(mads_array_t *array, void *data)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
+
+    // Insert the data at the beginning of the array.
     mads_array_insert_at(array, 0, data);
 }
 
-
+// The function that inserts data at a specific location in the array.
 void mads_array_insert_at(mads_array_t *array, const long long int index, void *data)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
+
+    // Check if the index is within the correct range. If it is not, throw an assertion error.
     assert(index >= 0 && index <= array->index + 1);
 
+    // Check if the array has reached its maximum capacity. If it has, double the size.
     if (array->index + 1 == array->msize)
     {
         const long long int double_msize = 2 * array->msize;
@@ -54,77 +92,112 @@ void mads_array_insert_at(mads_array_t *array, const long long int index, void *
         array->msize = double_msize;
     }
 
+    // Index increase for the new element.
     array->index++;
+
+    // Shift all elements to the right of the index one place to the right.
     for (long long int i = array->index; i > index; i--) { array->mblocks[i] = array->mblocks[i - 1]; }
+
+    // Insert the new element at the correct position.
     array->mblocks[index] = data;
 }
 
-
+// The function that removes the data at a specific position in the array.
 void mads_array_remove_at(mads_array_t *array, const long long int index)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
+
+    // Check if the index is within the correct range. If it is not, throw an assertion error.
     assert(index >= 0 && index <= array->index);
 
+    // If the destructor function is available, call it.
     if (array->destructor != NULL)
     {
         array->destructor(array->mblocks[index]);
         array->mblocks[index] = NULL;
     }
 
+    // Shift all elements to the right of the index one place to the left.
     for (long long int i = index; i < array->index; i++) { array->mblocks[i] = array->mblocks[i + 1]; }
+
+    // Index decrease after the deletion.
     array->index--;
 }
 
-
+// The function that updates the data at a specific position in the array.
 void mads_array_set_at(const mads_array_t *array, const long long int index, void *data)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
+
+    // Check if the index is within the correct range. If it is not, throw an assertion error.
     assert(index >= 0 && index <= array->index);
+
+    // If the destructor function is available, free the old data
     if (array->destructor != NULL) { array->destructor(array->mblocks[index]); }
+
+    // Set the new data at the correct position.
     array->mblocks[index] = data;
 }
 
-
+// The function that retrieves data from a specific position in the array.
 void *mads_array_get_at(const mads_array_t *array, const long long int index)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
+
+    // Check if the index is within the correct range. If it is not, throw an assertion error.
     assert(index >= 0 && index <= array->index);
+
+    // Return the data at the requested index.
     return array->mblocks[index];
 }
 
-
+// The function that searches for an item in the array and returns the index it was found at. If not found, it returns -1.
 long long int mads_array_has_element(const mads_array_t *array, const void *item)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
 
+    // Iterate over the array and use the comparator function to check if the item is in the array.
     for (long long int i = 0; i <= array->index; i++)
     {
         const int result = array->comparator(array->mblocks[i], item);
         if (result == 0) { return i; }
     }
 
+    // If the item was not found, return -1.
     return -1;
 }
 
-
+// The function that returns the current size of the array.
 long long int mads_array_size(const mads_array_t *array)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
+
+    // Return the current size of the array.
     return (array->index + 1);
 }
 
-
+// The function that checks if the array is empty.
 int mads_array_is_empty(const mads_array_t *array)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
+
+    // Check if the index is -1. If it is, return 1 indicating the array is empty. Otherwise, return 0.
     return (array->index == -1 ? 1 : 0);
 }
 
-
+// The function that clears the array by removing all elements.
 void mads_array_clear(mads_array_t *array)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
 
+    // If the destructor function is available, iterate over the array and free all elements using the destructor.
     if (array->destructor != NULL)
     {
         for (long long int i = 0; i <= array->index; i++)
@@ -134,20 +207,24 @@ void mads_array_clear(mads_array_t *array)
         }
     }
 
+    // Set the array's index to -1 (empty).
     array->index = -1;
 }
 
-
+// The function that prints all items in the array.
 void mads_array_print(const mads_array_t *array)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
 
+    // If the array is empty, print an empty bracket pair and return.
     if (mads_array_is_empty(array))
     {
         printf("[ ]\n");
         return;
     }
 
+    // Print the items in the array using the printer function.
     printf("[ ");
     for (long long int i = 0; i <= array->index; i++)
     {
@@ -157,11 +234,13 @@ void mads_array_print(const mads_array_t *array)
     printf("\b\b ]\n");
 }
 
-
+// The function that frees the mads array by removing all elements and then freeing the array itself.
 void mads_array_free(mads_array_t *array)
 {
+    // Check if the array is not NULL. If it is, throw an assertion error.
     assert(array != NULL);
 
+    // If the destructor function is available, iterate over the array and free all elements using the destructor.
     if (array->destructor != NULL)
     {
         for (long long int i = 0; i <= array->index; i++)
@@ -171,8 +250,11 @@ void mads_array_free(mads_array_t *array)
         }
     }
 
+    // Free the array blocks and set the pointer to NULL.
     free(array->mblocks);
     array->mblocks = NULL;
+
+    // Free the mads array and set the pointer to NULL.
     free(array);
     array = NULL;
 }
--- tests/mads/algorithms/test_sort.c
+++ tests/mads/algorithms/test_sort.c
@@ -1,12 +1,13 @@
+// ReSharper disable CppUnusedIncludeDirective
 #include <stdarg.h>
 #include <setjmp.h>
 #include <cmocka.h>
 
 #include <mads/algorithms/sort.h>
 
-static void test_sort_create(void **state)
+static void test_mads_bubble_sort(void **state)
 {
-    assert_int_equal(2, 2);
+    // TODO: write unit tests for bubble sort algorithm.
 }
 
 
@@ -14,7 +15,7 @@ int main()
 {
     const struct CMUnitTest tests[] =
     {
-        cmocka_unit_test(test_sort_create),
+        cmocka_unit_test(test_mads_bubble_sort),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
